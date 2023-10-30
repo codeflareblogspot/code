5296b7eb8676a5993fa806159a42c96ede028c3a
CodeFlare <54242341+codeflareblogspot@users.noreply.github.com>
2023-10-30 17:02:09 +0700

    Update cfl_infolink.js

--- cfl_infolink.js
+++ cfl_infolink.js
@@ -33,7 +33,7 @@ var tmpElm='<div id="dataInfoLink" style="top:-500px;left:-500px;"><div class="i
 $('body').append(tmpElm);
 for(var i=0;i<$(elmCflInfoLink).length;i++){
 var t=$(elmCflInfoLink).eq(i).attr('href').toLowerCase();
-var s=t.substring(t.lastIndexOf('/')+1);
+var s=t.substring(t.lastIndexOf('/')+1,t.lastIndexOf('.html'));
 $.ajax({url: 'https://'+window.location.hostname+'/feeds/posts/summary?q='+s+'&max-results=1&alt=json-in-script&callback=jsonLink',dataType: 'script',error: function (){console.log("Error JSON Codeflare InfoLink");}});
 $(elmCflInfoLink).eq(i).attr({'onmouseover':'getInfoLink('+i+')','onmousemove':'moveInfoLink(event)'});
 }}cflInfoLink();
