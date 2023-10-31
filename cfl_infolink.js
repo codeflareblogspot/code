099d62505bba2f23a7fccdf1e729b455bde001f1
CodeFlare <54242341+codeflareblogspot@users.noreply.github.com>
2023-10-31 23:30:27 +0700

    Update cfl_infolink.js

--- cfl_infolink.js
+++ cfl_infolink.js
@@ -1,7 +1,8 @@
 var infoLinkImg=[];
 var infoLinkTitle=[];
 var infoLinkDesc=[];
-var infoLinkUrl=[]
+var infoLinkUrl=[];
+var countJsonInfoLink=0;
 function getInfoLink(data){
 $('#dataInfoLink').hide();
 $('.imgInfo img').attr('src',infoLinkImg[data]);
@@ -21,23 +22,23 @@ if(l > ((w/2)-(elw/2)) && l < ((w/2)+(elw/2))){l=l-((elw+20)/2);}
 else if(l+elw > w){l=l-(elw+20);}
 $('#dataInfoLink').css({'top':t+'px','left':l+'px'});
 }
-  
+
 function callJsonFeed(x){
-if(infoLinkImg[x]==''||infoLinkImg[x]==undefined||infoLinkImg[x]==null||infoLinkImg[x].length < 0){
+if((infoLinkImg[x]==''||infoLinkImg[x]==undefined||infoLinkImg[x]==null||infoLinkImg[x].length < 0)&& infoLinkUrl[x]!=null){
 var path='https://'+window.location.hostname+infoLinkUrl[x];
 $.ajax({url: path,dataType: 'script',error: function (){console.log("Error JSON Codeflare InfoLink");}});
 countJsonInfoLink=countJsonInfoLink+1;
 }else{
 countJsonInfoLink=countJsonInfoLink+1;
 if(countJsonInfoLink != parseInt($(elmCflInfoLink).length)){callJsonFeed(countJsonInfoLink);}
-}
-}
-var countJsonInfoLink=0;
+}}
+
 function jsonLink(json){
 var ti='';var tx='';var tz='';var key='no';
 var entry=json.feed.entry;
-for(var j=0;j<$(elmCflInfoLink).length;j++){
-if(infoLinkImg[j]==''||infoLinkImg[j]==undefined||infoLinkImg[j]==null||infoLinkImg[j].length < 0){key=$(elmCflInfoLink).eq(j).attr('href').toLowerCase();}
+var co=0;
+$(elmCflInfoLink).each(function() {
+if(infoLinkImg[co]==''||infoLinkImg[co]==undefined||infoLinkImg[co]==null||infoLinkImg[co].length < 0){key=this.href.toLowerCase();}
 for(var i=0;i < entry.length;i++){
 var se=entry[i].link[4].href.toLowerCase();
 if(key==se){
@@ -46,25 +47,32 @@ ti=ti.replace("1.bp", "4.bp").replace("s72-c", "s300");
 tx=entry[i].summary.$t;
 if(tx.length > 100){tx=tx.slice(0, 100) + '...';}
 tz=entry[i].title.$t;
-infoLinkImg[j]=(ti);
-infoLinkDesc[j]=(tx);
-infoLinkTitle[j]=(tz);
-}
-}
-}
+infoLinkImg[co]=(ti);
+infoLinkDesc[co]=(tx);
+infoLinkTitle[co]=(tz);
+}}
+co++;
+});
 if(countJsonInfoLink != parseInt($(elmCflInfoLink).length)){
 callJsonFeed(countJsonInfoLink);
 }}
+
 function cflInfoLink(){
 $(elmCflInfoLink).mouseleave(function(){$('#dataInfoLink').hide();});
 var tmpElm='<div id="dataInfoLink" style="top:-500px;left:-500px;"><div class="imgInfo"><img/></div><div class="titleInfo"></div><div class="descInfo"></div></div>';
 $('body').append(tmpElm);
-for(var i=0;i<$(elmCflInfoLink).length;i++){
-var t1=$(elmCflInfoLink).eq(i).attr('href').toLowerCase();
-var t2=t1.split( '/' );var m=t2[3];var d=t2[4];
+var co=0;
+$(elmCflInfoLink).each(function() {
+var t1=this.href.toLowerCase();
+var t2=t1.split( '/' );var m=parseInt(t2[3]);var d=t2[4];
+var mt = /^\d{4}$/.test(m);
+let ms = /.html#/.test(t1);
+if(typeof m === 'number' && mt == true && ms == false){
 infoLinkUrl[i]='/feeds/posts/summary?published-min='+m+'-'+d+'-01T00:00:00&published-max='+m+'-'+d+'-31T23:59:59&max-result=150&alt=json-in-script&callback=jsonLink';
-$(elmCflInfoLink).eq(i).attr({'onmouseover':'getInfoLink('+i+')','onmousemove':'moveInfoLink(event)'});
-}
+this.setAttribute('onmouseover','getInfoLink('+co+')');
+this.setAttribute('onmousemove','moveInfoLink(event)');
+}else{infoLinkUrl[i]=null;}
+co++;
+});
 callJsonFeed(countJsonInfoLink);
-}
-cflInfoLink();
+}cflInfoLink();
