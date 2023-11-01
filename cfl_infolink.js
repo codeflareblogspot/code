925e0ca2098169bb8d9325be96ed4e25a8a108ac
CodeFlare <54242341+codeflareblogspot@users.noreply.github.com>
2023-11-01 15:52:17 +0700

    Update cfl_infolink.js

--- cfl_infolink.js
+++ cfl_infolink.js
@@ -48,7 +48,7 @@ $(elmCflInfoLink).each(function(j) {
 if(infoLinkImg[j]==''||infoLinkImg[j]==undefined){var key=this.href.toLowerCase();}else{var key=null;}
 for(var k=0;k < entry.length;k++){
 var se=entry[k].link[4].href.toLowerCase();
-if(se==undefined&&tryLoadFeed==false){tryLoadFeed=true;callJsonFeedError(countJsonInfoLink);break;return;}
+if(se==undefined&&tryLoadFeed==false){tryLoadFeed=true;callJsonFeedError(countJsonInfoLink);break;return;}else{
 if(key==se&&se!=undefined){
 ti=entry[k].media$thumbnail.url;
 ti=ti.replace("1.bp", "4.bp").replace("s72-c", "s300");
@@ -58,7 +58,7 @@ tz=entry[k].title.$t;
 infoLinkImg[j]=(ti);
 infoLinkDesc[j]=(tx);
 infoLinkTitle[j]=(tz);
-}}
+}}}
 });
 if(countJsonInfoLink != infoLinkUrl.length - 1){countJsonInfoLink=countJsonInfoLink+1;callJsonFeed(countJsonInfoLink);}
 }
