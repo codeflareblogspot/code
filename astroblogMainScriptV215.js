6eb91fc2794e27f3b1ca8691c9c1abffc369c240
CodeFlare <54242341+codeflareblogspot@users.noreply.github.com>
2024-06-18 12:00:53 +0800

    Update astroblogMainScriptV215.js

--- astroblogMainScriptV215.js
+++ astroblogMainScriptV215.js
@@ -161,7 +161,6 @@ res = str.substring(str.lastIndexOf(str.match(name))+5,str.length);
 initialDate=res.split(";");
 initialDate[0]=window.atob(initialDate[0]);
 initialDate[1]=window.atob(initialDate[1]);
-getBioImage();
 }else{initialDate=getCookie("data").split(":");}
 if(initialDate[0]!=""&&initialDate[0]!=null){$("#fAstName").val(initialDate[0]);getArtiNama();}
 if(initialDate[1]!=""&&initialDate[1]!=null){$("#fAstDate").datepicker("setDate",initialDate[1]);$("#fBtnAstSave").css("background","#333");}
@@ -175,6 +174,7 @@ function (){$("#titleHeaderAst").html(contentAPF[initialDate[2]]+' '+contentAPT[
 $("#"+contentAPId[contentAPN2]).delay(800).show("slide", 500);
 $("#titleHeaderAst").delay(300).show("slide", 500);
 contentAPN1=parseInt(contentAPN2);
+getBioImage();
 }
 });
 }
