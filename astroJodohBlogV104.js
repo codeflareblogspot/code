7855741495ec2d25ce4355e724a9ad906a1bcf7f
CodeFlare <54242341+codeflareblogspot@users.noreply.github.com>
2024-06-20 15:05:18 +0800

    Update astroJodohBlogV104.js

--- astroJodohBlogV104.js
+++ astroJodohBlogV104.js
@@ -87,8 +87,9 @@ $("#titleHeaderJodoh").hide("slide",{direction : 'right'}, 500,
 function (){$("#titleHeaderJodoh").html(contentAPFJodoh[initialDate[4]]+' '+contentAPTJodoh[initialDate[4]]);});
 $("#titleHeaderJodoh").delay(300).show("slide", 500);
 contentAPN1Jodoh=contentAPN2Jodoh;
-//alert("Kalkulasi Jodoh Online\n\n"+initialDate[0]+" <> "+initialDate[2]);
-alert("Test");
+}
+if(str.match(name)){
+alert("Kalkulasi Jodoh Online\n\n"+initialDate[0]+" | "+initialDate[2]);
 $('html, body').animate({ scrollTop: $(".astroBlogMainUIJodoh").offset().top }, 500);
 }
 }
