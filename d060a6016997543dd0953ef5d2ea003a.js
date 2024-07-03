189066dcba58f2d91bc269c02e05ae6d52de29e1
CodeFlare <54242341+codeflareblogspot@users.noreply.github.com>
2024-07-03 21:10:57 +0800

    Update d060a6016997543dd0953ef5d2ea003a.js

--- d060a6016997543dd0953ef5d2ea003a.js
+++ d060a6016997543dd0953ef5d2ea003a.js
@@ -1,4 +1,9 @@
-function openUserData(){
+function openUserData(x){
+var oathUser=null;
 var activeUser = [];
 activeUser[0]="ecd5ef267c08834301797c76842d223b";
+for(var i=0;i<activeUser.length;i++){
+if(x==activeUser[i]){oathUser="authorizeUser";break;}
+}
+return oathUser;
 }
