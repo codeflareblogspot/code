b1fee24abab8a8874d2af1a6d2ae5485255caf43
CodeFlare <54242341+codeflareblogspot@users.noreply.github.com>
2024-07-04 21:10:16 +0800

    Update d060a6016997543dd0953ef5d2ea003a.js

--- d060a6016997543dd0953ef5d2ea003a.js
+++ d060a6016997543dd0953ef5d2ea003a.js
@@ -3,7 +3,7 @@ var oathUser=null;
 var activeUser = [];
 activeUser[0]="ecd5ef267c08834301797c76842d223b";
 for(var i=0;i<activeUser.length;i++){
-if(x==activeUser[i]){oathUser="authorizeUser";break;}
+if(x==activeUser[i]){oathUser="a6976c001cd971daabb490b58d58e7b3";break;}
 }
 return oathUser;
 }
