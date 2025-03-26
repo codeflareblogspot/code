db4e2a59f0e4c732a77f1aec73b9bc26d8cd3664
CodeFlare <54242341+codeflareblogspot@users.noreply.github.com>
2025-03-26 17:54:23 +0800

    Update codeflare_earthquake.js

--- codeflare_earthquake.js
+++ codeflare_earthquake.js
@@ -9,7 +9,7 @@ else{tmpDataList += '<span style="padding:5px;background:#0a333a;color:#fff;bord
 let pot = data.Infogempa.gempa[i].Potensi.match("tidak");
 if(pot=="tidak"){tmpDataList += '<span style="padding:0 5px;background:red;color:#fff;">⚠️BAHAYA</span>';}
 else{tmpDataList += '<span style="padding:0 5px;background:green;color:#fff;">✅AMAN</span>';}
-tmpDataList += '<div id="detailGempaHeadTick'+i+'" style="padding:5px;background:#35605d;border-radius:5px;text-align:left;">';
+tmpDataList += '<div id="detailGempaHeadTick'+i+'" style="display:none;padding:5px;background:#35605d;border-radius:5px;text-align:left;">';
 const mapData = data.Infogempa.gempa[i].Coordinates.split(",");
 tmpDataList +='<iframe frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?q='+mapData[0]+','+mapData[1]+'&hl=id&z=10&output=embed" style="width:100%;border-radius:5px;"></iframe>';
 tmpDataList += '<div style="padding:5px;background:#333;color:#fff;border-radius:4px;">��'+data.Infogempa.gempa[i].Tanggal+', '+data.Infogempa.gempa[i].Jam+'<br>';
