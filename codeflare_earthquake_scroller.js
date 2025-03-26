e7dfcb83575775d61f11dc0284c119f140a45b1a
CodeFlare <54242341+codeflareblogspot@users.noreply.github.com>
2025-03-26 18:22:26 +0800

    Update codeflare_earthquake_scroller.js

--- codeflare_earthquake_scroller.js
+++ codeflare_earthquake_scroller.js
@@ -4,8 +4,8 @@ var dataTime=10;  //Timer to Replace Data in Second
 function posGempaHeadTick(event,id) {
 var w = document.querySelector(elmCflBoundary).offsetWidth;
 var h = window.innerHeight || html.clientHeight  || body.clientHeight  || screen.availHeight;
-var elw = document.querySelector('.scroller').offsetWidth;
-var elh = document.querySelector('.scroller').offsetHeight;
+var elw = document.querySelector('.dataInfoGempaHeadTick').eq(id).offsetWidth;
+var elh = document.querySelector('.dataInfoGempaHeadTick').eq(id).offsetHeight;
 var l=event.clientX + 10;
 var t=event.clientY + 30;
 if((t + elh + 30) > h){t=t - elh - 40;}
