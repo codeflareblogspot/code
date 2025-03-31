20ad354269ddbb1f63040dcda30c6f02bc51b2d5
CodeFlare <54242341+codeflareblogspot@users.noreply.github.com>
2025-03-31 19:09:10 +0800

    Update astroblogMainScriptV215.js

--- astroblogMainScriptV215.js
+++ astroblogMainScriptV215.js
@@ -197,29 +197,27 @@ $("#fBtnAstSave").on("click", function(){checkCookie();});
 $("#fBtnAstClear").on("click", function(){deleteCookie();});
 $("#fBtnAstShare").on("click", function(){
 var username = $('#fAstName').val();
-if (username != "" && username != null && username !="Nama Lengkap" && username.length != 1) {
+if (username != "" && username != null && username !="Nama Lengkap" && username.length != 1){
+alert("Silakan ketik nama anda terlebih dahulu dan pilih tanggal lahir anda !");
+$("#fAstName").trigger( "focus" );return;
+}
 var dataAstrologi=(window.location.hostname)+"#data="+(window.btoa($('#fAstName').val()))+";"+(window.btoa($('#fAstDate').val()))+";"+contentAPN1;
 var a = 'ASTROLOGI reading for '+escape($('#fAstName').val())+'%0A%0ADetail Source :%0A'+escape(dataAstrologi)+'%0A%0ASend from :%0A'+escape(window.location.hostname);
 var shareText = 'ASTROLOGI reading for '+escape($('#fAstName').val())+'%0A%0ADetail Source :%0A'+escape(dataAstrologi);
-if (navigator.canShare) {
-try {
-        await navigator.share({'', 'codeflare.net', shareText, window.location.hostname});
-        console.log('Successfully sent share');
-      } catch (error) {
-        console.log('Error sharing: ' + error);
-	if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){var addQuotes = 'whatsapp://send?phone=&text='+a;}
-	else{var addQuotes = 'https://web.whatsapp.com/send?phone=&text='+a;}
-	chkBrowserAst(addQuotes,800,600);
-      }
-}else{
+if (navigator.canShare){
+try {await navigator.share({'', 'codeflare.net', shareText, window.location.hostname});
+console.log('Successfully sent share');
+}
+catch (error){
+console.log('Error sharing: ' + error);
+if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){var addQuotes = 'whatsapp://send?phone=&text='+a;}else{var addQuotes = 'https://web.whatsapp.com/send?phone=&text='+a;}
+chkBrowserAst(addQuotes,800,600);
+}}else{
 if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){var addQuotes = 'whatsapp://send?phone=&text='+a;}
 else{var addQuotes = 'https://web.whatsapp.com/send?phone=&text='+a;}
 chkBrowserAst(addQuotes,800,600);
-}}
-else{
-alert("Silakan ketik nama anda terlebih dahulu dan pilih tanggal lahir anda !");
-$("#fAstName").trigger( "focus" );
-}});
+}
+});
 function chkBrowserAst(e,w,h){
 var chkAgent=navigator.userAgent.toLowerCase();
 var isChkMobile = 
